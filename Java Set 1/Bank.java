90136428e8a4c1c0fe8118aada9906316e9b120a
shubhamchaudhary1718 <23dcs015@charusat.edu.in>
2024-07-30 09:28:54 +0530

    Create Bank.java

---
+++ Java Set 1/Bank.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Bank{
+public static void main(String args[]){
+int a=20;
+System.out.println("Current Balance:$"+a);
+System.out.println("23DCS015 SHUBHAM CHAUDHARY");
+}
+}
