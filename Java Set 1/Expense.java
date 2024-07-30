8688147b7fa9457933beae47583296a984f9f4ff
shubhamchaudhary1718 <23dcs015@charusat.edu.in>
2024-07-30 09:33:37 +0530

    Create Expense.java

---
+++ Java Set 1/Expense.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Expense{
+public static void main(String args[]){
+int day[]=new int[30];
+Scanner sc=new Scanner(System.in);
+int sum=0;
+for(int i=0;i<5;i++){
+System.out.print("Day"+(i+1)+":");
+int a=sc.nextInt();
+sum+=a;
+}
+System.out.println("total Expenses:"+sum);
+System.out.println("23DCS015 SHUBHAM CHAUDHARY");
+}
+}
