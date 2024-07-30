ebd49acd7dfc56d34fa0d9cacda3a0b53684fe7e
shubhamchaudhary1718 <23dcs015@charusat.edu.in>
2024-07-30 09:22:05 +0530

    Delete Bank.java

--- Bank.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class Bank{
-public static void main(String args[]){
-int a=20;
-System.out.println("Current Balance:$"+a);
-System.out.println("23DCS015 SHUBHAM CHAUDHARY");
-}
-}
