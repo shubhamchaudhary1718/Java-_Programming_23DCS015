f81bd0bd15cea6e65e95b807200a20a6ed5a81af
shubhamchaudhary1718 <23dcs015@charusat.edu.in>
2024-08-06 10:31:12 +0530

    Create Prac20.java

---
+++ Java Set 4/Prac20.java
@@ -0,0 +1,36 @@
+
+class Shape {
+    void printShape() {
+        System.out.println("This is shape");
+    }
+}
+
+class Rectangle extends Shape {
+    void printRectangle() {
+        System.out.println("This is rectangular shape");
+    }
+}
+
+class Circle extends Shape {
+    void printCircle() {
+        System.out.println("This is circular shape");
+    }
+}
+
+
+class Square extends Rectangle {
+    void printSquare() {
+        System.out.println("Square is a rectangle");
+    }
+}
+
+
+public class Prac20{
+    public static void main(String[] args) {
+      
+        Square square = new Square();
+        square.printShape();         
+        square.printRectangle();
+     
+    }
+}
