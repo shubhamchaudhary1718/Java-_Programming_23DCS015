a39db8659f2c60a405d4f8e1dcb259fa66ea0d69
shubhamchaudhary1718 <23dcs015@charusat.edu.in>
2024-09-17 10:22:02 +0530

    Create prec35.java

---
+++ Java Set 7/prec35.java
@@ -0,0 +1,27 @@
+class IncrementThread extends Thread {
+    private int number;
+
+    public IncrementThread(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            number++;
+            System.out.println("Incremented value after 1 second: " + number);
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted");
+        }
+    }
+}
+
+public class prec35 {
+    public static void main(String[] args) {
+        int initialValue = 0;
+        System.out.println("Initial value: " + initialValue);
+        IncrementThread incrementThread = new IncrementThread(initialValue);
+        incrementThread.start();
+    }
+}
